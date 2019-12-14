b41b8c6c6c6863f3e9a243c01b0367f2b3b24534
yukikatsumura <yuhki0919@gmail.com>
2019-12-15 01:20:28 +0900

    initial commit

---
+++ msa_hystrix/src/main/java/com/example/msa_hystrix/MsaHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa_hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaHystrixApplication.class, args);
+	}
+
+}
---
+++ msa_hystrix/src/test/java/com/example/msa_hystrix/MsaHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaHystrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
